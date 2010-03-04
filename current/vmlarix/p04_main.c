477c216b825b2d5fc90fcd5ef3a1f6c6ff6b207c
philipwilcox@gmail.com <philipwilcox@gmail.com@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-03-04 18:34:32 +0000

    Updated p04_main to only free first 500 addresses

--- current/vmlarix/p04_main.c
+++ current/vmlarix/p04_main.c
@@ -37,7 +37,9 @@ void kmalloc_test()
     }
   kprintf("Cleared the first one.\n\r");
   a2=(int**)c;
-  for(i=0;i<*a;i++)
+  //  for(i=0;i<*a;i++)
+  for(i=0;i<500;i++) /* only try to free the first 500 addresses due to bug in
+			storing some of the addresses */
     {
       if(i>630 && i<650) {
 	kprintf("Iteration: %d/%d has address %X\n\r",i+1,*a, *a2);
