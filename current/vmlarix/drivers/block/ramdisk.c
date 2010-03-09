f20430cfe5b76aa17c29f921fdb06e5afaf8171d
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-03-09 17:39:39 +0000

    added project 5 source tree

---
+++ current/vmlarix/drivers/block/ramdisk.c
@@ -0,0 +1,75 @@
+#include <stddef.h> 
+#include <blkdev.h> 
+#include <ramdisk.h> 
+#include <stdint.h>
+#include <byteswap.h>
+
+#ifndef _KERNEL_
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define kmalloc malloc
+#define kprintf printf
+#else
+#include <misc.h>
+#endif
+
+#ifdef PTR64
+typedef uint64_t ptrint_t;
+#else
+typedef uint32_t ptrint_t;
+#endif
+
+char ramdisk_magic[]="VMLARIX RAMDISK";
+
+typedef struct rm{
+  int num;             /* minor number for this RAMdisk */
+  char *data;          /* start of data for this RAMdisk */
+  uint32_t blocksize;  /* size of a block, in bytes */
+  uint32_t length;     /* number of blocks */
+  uint32_t bitshift;   /* number of bits to shift for indexing */
+  struct rm *next;     /* pointer to next minor number */
+}ramdisk_minor;
+
+ramdisk_minor *minors;
+
+/* Takes a pointer and looks to see if there is a RAMdisk there.
+   If so, return the size of the RAMdisk, in bytes. Returns 0 on
+   failure.
+*/
+int32_t ramdisk_attach(char *mem_start)
+{
+}
+
+void ramdisk_detach(uint16_t minor)
+{
+}
+
+int32_t ramdisk_read(uint16_t minor,
+		     uint32_t block,
+		     char *buffer,
+		     uint32_t nblocks)
+{
+}
+
+int ramdisk_write(uint16_t minor,
+		   uint32_t block,
+		   char *buffer,
+		   uint32_t nblocks)
+{
+}
+
+int ramdisk_num_blk(uint16_t minor)
+{
+}
+
+int ramdisk_block_size(uint16_t minor)
+{
+}
+
+/* initialize the datastructures needed to manage ramdisks */
+void ramdisk_init(block_device *blk_dev)
+{
+  kprintf("RAMdisk driver initialized.\n\r");
+}
+
---
+++ current/vmlarix/p04_main.c
@@ -0,0 +1,106 @@
+
+#include <sys/types.h>
+#include <kprintf.h>
+#include <phys_mem.h>
+#include <dev_list.h>
+#include <mmu.h>
+#include <slabs.h>
+#include <linker_vars.h>
+#include <kmalloc.h>
+
+uint16_t console_major;
+uint16_t console_minor;
+
+
+void kmalloc_test()
+{
+  int *a,*b,*c,**a2,i,freed;
+
+  a = kmalloc(sizeof(int));
+  *a=1000;                 
+  c = kmalloc((*a)*sizeof(int));
+  kprintf("Size of a and c %d\n\r",10*sizeof(int));
+  kprintf(" value of a %d\n\r",*a);
+  kprintf("%X\n\r",a);
+  kprintf("%X\n\r",c);
+
+  a2=(int**)c;
+  for(i=0;i<*a;i++)
+    {
+      *a2=kmalloc(2048);
+      a2+=sizeof(int);
+    }
+  a2=(int**)c;
+  for(i=0;i<*a;i++)
+    {
+      kfree(*a2);
+      a2+=sizeof(int);
+    }
+
+  b = kmalloc(4096);
+  kprintf("%X\n\r",b);
+  kfree(a);
+  kfree(b);
+  kfree(c);
+
+  freed=kmalloc_free_some_pages();
+  kprintf("I freeded the people and %d pages\n\r",freed);
+  /* do a lot more testing here */
+
+}
+
+#define BUFF_LEN 100
+int main()
+{
+  uint32_t i;
+  uint32_t j;
+  uint32_t pages_free;
+  char buffer[BUFF_LEN];
+  phys_mem_t kernel_pt;
+
+  arch_init();
+
+  char_dev_init(char_dev_list);
+
+  kprintf("Kernel start: %X\n\r",__kernel_ram_start__);
+  kprintf("Kernel end: %X\n\r",__kernel_ram_end__);
+
+  kprintf("Initializing memory\n\r");
+  phys_mem_init(); 
+  kprintf("There are %d pages free\n\r",phys_mem_count_free());
+
+  kprintf("Setting up kernel page table\n\r");
+  kernel_pt = setup_kernel_page_table();
+  kprintf("There are %d pages free\n\r",phys_mem_count_free());
+
+  kprintf("Setting up slab allocator\n\r");
+  slab_init();
+
+  kprintf("Loading MMU\n\r");
+  set_initial_page_table(kernel_pt);
+
+  kprintf("Enabling MMU\n\r");
+  enable_mmu();
+  kprintf("MMU enabled\n\r");
+
+  kprintf("Setting up the stacks\n\r");
+  setup_stacks();
+
+  kprintf("Initializing kmalloc\n\r");
+  kmalloc_init();
+
+  kprintf("Testing kmalloc\n\r");
+  kmalloc_test();
+
+  kprintf("Hello out there!\n\r");
+  kprintf("Please type something to check if I can read it.\n\r");
+
+  i=0;
+  char_read(console_major,console_minor,&(buffer[i]),1);
+  while((buffer[i]!='\r')&&(i<BUFF_LEN-1))
+    char_read(console_major,console_minor,&(buffer[++i]),1);
+  buffer[i]=0;
+  kprintf("\nYou typed \'%s\' \n\r",buffer);
+
+  while(1);
+}
