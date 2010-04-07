eae7f161dc556acd2b8a13f7fbcc4a5ea8140dd5
philipwilcox@gmail.com <philipwilcox@gmail.com@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-07 22:14:26 +0000

    Added additional fstat sfs-specific code

--- current/vmlarix/filesystem/sfs/sfs_fstat.c
+++ current/vmlarix/filesystem/sfs/sfs_fstat.c
@@ -25,16 +25,16 @@ int sfs_fstat(filedesc *f, struct fstat *buf)
 {
   kprintf("sfs_fstat() function not implemented\n\r");
   /* TODO: populate the following fields of "buf":
-  /* dev_t     st_dev;     /\* ID of device containing file *\/ */ 
+  //   /* dev_t     st_dev;     /\* ID of device containing file *\/ */ 
   //   /* ino_t     st_ino;     /\* inode number *\/ */
   //   /* mode_t    st_mode;    /\* protection *\/ */
   //   /* nlink_t   st_nlink;   /\* number of hard links *\/ */
   //   /* uid_t     st_uid;     /\* user ID of owner *\/ */
   //   /* gid_t     st_gid;     /\* group ID of owner *\/ */
-  /* dev_t     st_rdev;    /\* device ID (if special file) *\/ */
+  //   /* dev_t     st_rdev;    /\* device ID (if special file) *\/ */
   //   /* off_t     st_size;    /\* total size, in bytes *\/ */
   //   /* blksize_t st_blksize; /\* blocksize for file system I/O *\/ */
-  /* blkcnt_t  st_blocks;  /\* number of 512B blocks allocated *\/ */
+  /* blkcnt_t  st_blocks;  /\* number of blocks allocated *\/ */
   //   /* time_t    st_atime;   /\* time of last access *\/ */
   //   /* time_t    st_mtime;   /\* time of last modification *\/ */
   //   /* time_t    st_ctime;   /\* time of last fstatus change *\/ */
@@ -42,8 +42,9 @@ int sfs_fstat(filedesc *f, struct fstat *buf)
   buf->st_mode = f->mode;
   /* get blocksize for FS from superblock from private FS data */
   buf->st_blocksize = ((sfs_fd_private *)f->fs_private)->sb->block_size;
-  /* TODO: what do I need to do for number of 512B blocks allocated? Does that
-     mean allocated just for this file? If so, is this just division? */
+  /* TODO: what do I need to do for number of blocks allocated? Does that mean
+     allocated just for this file? If so, is this just division
+     (size/blocksize)? */
 
   /* get inode pointer and inode number from file descriptor */
   sfs_inode_t *inode = ((sfs_fd_private *)f->fs_private)->inode;
@@ -56,9 +57,14 @@ int sfs_fstat(filedesc *f, struct fstat *buf)
   buf->st_uid = inode->owner;
   buf->st_gid = inode->group;
   buf->st_size = inode->size;
-  /* TODO: where does 'ID of device containing file' come from? major/minor
-     numbers can be retrieved from fieldesc->mp->major/minor, but are those
-     what go here? And which one? */
-  /* TODO: also, what about 'st_rdev' device ID for special files? */
+  /* Shift major number left and add minor number to get device number */
+  uint32_t major = f->mp->major;
+  uint32_t minor = f->mp->minor;
+  /* TODO: I'm assuming here that dev_t is a 64-bit type; double-check this */
+  buf->st_dev = (major<<32) + minor;
+  /* handle device number for special files */
+  if(inode->type==FT_CHAR_SPEC || inode->type==FT_BLOCK_SPEC) {
+    buf->st_rdev = (inode->direct[0] << 32) + inode->direct[1];
+  }
   
 }
