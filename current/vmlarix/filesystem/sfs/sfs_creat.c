4a88aa309534486ceaa2769d4e8f30147c6b50ff
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-08 17:02:21 +0000

    copied code from open

--- current/vmlarix/filesystem/sfs/sfs_creat.c
+++ current/vmlarix/filesystem/sfs/sfs_creat.c
@@ -25,12 +25,17 @@ int sfs_creat(void *fs_private, char *path, mode_t mode)
 {
   kprintf("sfs_creat() function not implemented\n\r");
 
-  sfs_mp_private *p = (sfs_mp_private *)fs_private;
+  sfs_inode_t *inode;
+  int32_t inum;
+
+  sfs_mp_private *mp = (sfs_mp_private *)fs_private;
   /* NOTE: he said in class that a lot of the stuff we need to do here is
      already implemnted in 'open'. On first reading, it looks like open creates
      a file if we try to open a file that does not exist. */
   /* I.e., the 'find the parent directory path' code from 'open' will work here
      as-is */
+
+  /* NOTE: How do we get the FD from fs_private? */
 
   /* NOTE: Copied from sfs_open.c */
 
@@ -67,9 +72,9 @@ int sfs_creat(void *fs_private, char *path, mode_t mode)
 
   pinode = (sfs_inode_t*)kmalloc(sizeof(sfs_inode_t));
   if(emptyparent)
-    pinum = sfs_lookup(p,"",pinode);
+    pinum = sfs_lookup(mp,"",pinode);
   else
-    pinum = sfs_lookup(p,ppath,pinode);
+    pinum = sfs_lookup(mp,ppath,pinode);
 
   if((pinode->type != FT_DIR))
     {
