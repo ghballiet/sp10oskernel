18ead32923c6f02b4c02bbbe18e95002ece979e3
philipwilcox@gmail.com <philipwilcox@gmail.com@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-09 16:16:28 +0000

    Added a lot of comments to sfs_lseek to guide implementation

--- current/vmlarix/filesystem/sfs/sfs_lseek.c
+++ current/vmlarix/filesystem/sfs/sfs_lseek.c
@@ -23,5 +23,27 @@
 
 int sfs_lseek(filedesc *f, off_t offset, int whence)
 {
+  /* From man file:
+     If whence is SEEK_SET, offset is set to 'offset' bytes
+
+     If whence is SEEK_CUR, offset is set to current offset plus 'offset' bytes
+
+     If whence is SEEK_END, offset is set to the size of the file plus 'offset'
+     bytes
+  */
+
+  /* So in the SEEK_END case, are we supposed to pad the file with 0s up to the
+     new offset location? */
+
+  /* Updating f->filepos is trivial; updated f->bufpos is harder (we may have
+     to find/load a different block) */
+  /* We can call sfs_fstat to populate an fstat structure to get
+     fstat->st_blksize instead of directly having to get this from the private
+     fs data -> superblock */
+  /* We have sfs_log2phys which takes a logical file block number and returns
+     the filesystem block number for that block (or 0 on failure) */
+  /* There is code in sfs_write that deals with writing the current buffer and
+     getting a new buffer that I can borrow from for writing out the current
+     block if it's dirty. */
   kprintf("sfs_lseek() function not implemented\n\r");
 }
