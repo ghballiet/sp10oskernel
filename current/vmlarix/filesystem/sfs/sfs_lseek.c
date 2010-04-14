150b06d48f266a1b10613f60a2221296bb50634f
philipwilcox@gmail.com <philipwilcox@gmail.com@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-14 18:25:59 +0000

    Working on lseek block handling

--- current/vmlarix/filesystem/sfs/sfs_lseek.c
+++ current/vmlarix/filesystem/sfs/sfs_lseek.c
@@ -80,6 +80,7 @@ int sfs_lseek(filedesc *f, off_t offset, int whence)
 
     /* NOTE: the call to sfs_write appears to update f->filepos and
        f->curr_log, but not f->bufpos */
+    kprintf("lseek: will set bufpos = %d\r\n",newpos - (f->curr_log * blksize));
     f->bufpos = newpos - (f->curr_log * blksize);
   } else {
     /* move the file pointer to the now inside-the-file location */
