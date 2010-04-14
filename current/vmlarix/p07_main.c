e1df058b85c3b7c90cd31caf4f89b9f4381054a0
philipwilcox@gmail.com <philipwilcox@gmail.com@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-14 17:41:14 +0000

    Modified lseek block-handling code

--- current/vmlarix/p07_main.c
+++ current/vmlarix/p07_main.c
@@ -186,6 +186,7 @@ int main()
   kprintf("current logical block=%d, bufpos=%d, filepos=%d\r\n", f2->curr_log,
 	  f2->bufpos, f2->filepos);
   kprintf("seeking to byte 130 (131st byte)\r\n");
+  vfs_lseek(fd2, 128, SEEK_SET);
   vfs_lseek(fd2, 130, SEEK_SET);
   vfs_fstat(fd, &buf2);
   kprintf("/newfile size=%d\r\n", buf2.st_size);
