a3a6296cccda647fbed8ddf0deda76866df129b9
philipwilcox@gmail.com <philipwilcox@gmail.com@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-14 18:17:26 +0000

    Modified lseek block-handling code

--- current/vmlarix/filesystem/sfs/sfs_lseek.c
+++ current/vmlarix/filesystem/sfs/sfs_lseek.c
@@ -80,51 +80,51 @@ int sfs_lseek(filedesc *f, off_t offset, int whence)
 
     /* TODO: double check that the call to sfs_write leaves the file pointer
        pointing to the new end of the file */
-  }
-  /* move the file pointer to the now inside-the-file location */
-  if(f->curr_log * blksize <= newpos &&
-     (f->curr_log + 1) * blksize > newpos) {
-    /* if we're moving to a different point in the current block */
-    /* NOTE: I'm assuming here that blksize/bufsize will always be the same */
-    /* kprintf("mode1: newpos=%d\r\n", newpos); */
-    f->bufpos = newpos - (f->curr_log * blksize);
-    /* kprintf("mode1: f->curr_log=%d\r\n", f->curr_log); */
-    /* kprintf("mode1: f->bufpos=%d\r\n", f->bufpos); */
   } else {
-    /* if we're moving to a different block */
-    if(f->dirty) {
-      blk_dev[f->major].write_fn(f->minor,
-				 f->curr_blk,
-				 f->buffer,
-				 fp->sb->sectorsperblock);
-      f->dirty=0;
+    /* move the file pointer to the now inside-the-file location */
+    if(f->curr_log * blksize <= newpos &&
+       (f->curr_log + 1) * blksize > newpos) {
+      /* if we're moving to a different point in the current block */
+      /* NOTE: I'm assuming here that blksize/bufsize will always be the same */
+      /* kprintf("mode1: newpos=%d\r\n", newpos); */
+      f->bufpos = newpos - (f->curr_log * blksize);
+      /* kprintf("mode1: f->curr_log=%d\r\n", f->curr_log); */
+      /* kprintf("mode1: f->bufpos=%d\r\n", f->bufpos); */
+    } else {
+      /* if we're moving to a different block */
+      if(f->dirty) {
+	blk_dev[f->major].write_fn(f->minor,
+				   f->curr_blk,
+				   f->buffer,
+				   fp->sb->sectorsperblock);
+	f->dirty=0;
     }
-    /* so get logical block number we want, and calculate the offset in it */
-    uint32_t logblk = newpos/blksize;
-    /* NOTE: in sfs_write this is calculated by dividing by f->bufsize (line
-       94); I'm assuming here that these two will always be the same */
-    uint32_t buf_offset = newpos - (logblk * blksize);
-    /* get filesystem block number for that block */
-    uint32_t fsblk = sfs_log2phys(f, logblk);
-    if(fsblk==0) {
-      /* something went wrong */
-      return -1;
+      /* so get logical block number we want, and calculate the offset in it */
+      uint32_t logblk = newpos/blksize;
+      /* NOTE: in sfs_write this is calculated by dividing by f->bufsize (line
+	 94); I'm assuming here that these two will always be the same */
+      uint32_t buf_offset = newpos - (logblk * blksize);
+      /* get filesystem block number for that block */
+      uint32_t fsblk = sfs_log2phys(f, logblk);
+      if(fsblk==0) {
+	/* something went wrong */
+	return -1;
+      }
+      /* load that filesystem block into buffer */
+      blk_dev[f->major].read_fn(f->minor,
+				fsblk,
+				f->buffer,
+				fp->sb->sectorsperblock);
+      /* finally, update file descriptor */
+      f->curr_log = logblk;
+      f->curr_blk = fsblk;
+      f->bufpos = buf_offset;
+      /* kprintf("mode2: newpos=%d\r\n", newpos); */
+      /* kprintf("mode2: f->curr_log=%d\r\n", f->curr_log); */
+      /* kprintf("mode2: f->bufpos=%d\r\n", f->bufpos); */
     }
-    /* load that filesystem block into buffer */
-    blk_dev[f->major].read_fn(f->minor,
-			      fsblk,
-			      f->buffer,
-			      fp->sb->sectorsperblock);
-    /* finally, update file descriptor */
-    f->curr_log = logblk;
-    f->curr_blk = fsblk;
-    f->bufpos = buf_offset;
-    /* kprintf("mode2: newpos=%d\r\n", newpos); */
-    /* kprintf("mode2: f->curr_log=%d\r\n", f->curr_log); */
-    /* kprintf("mode2: f->bufpos=%d\r\n", f->bufpos); */
+    f->filepos = newpos;
+    /* kprintf("end of lseek: newpos=%d\r\n", newpos); */
+    /* kprintf("end of lseek: f->filepos=%d\r\n", f->filepos); */
   }
-  f->filepos = newpos;
-  /* kprintf("end of lseek: newpos=%d\r\n", newpos); */
-  /* kprintf("end of lseek: f->filepos=%d\r\n", f->filepos); */
 }
-
