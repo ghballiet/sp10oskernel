26aa21ce7557c7d890a09f7234ea286f2e6c0de6
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-22 19:28:28 +0000

    added comments to vfs_open_dev to clarify what it should do

--- current/vmlarix/filesystem/vfs/vfs_open_dev.c
+++ current/vmlarix/filesystem/vfs/vfs_open_dev.c
@@ -30,3 +30,13 @@ lags) */
 /*   return i; */
 
 /* } */
+
+// COMMENTS 04.22.10
+// function vfs_open_dev should be used
+// to create system file descriptors referring to 
+// the console device. 
+// i.e. during process setup, this function should
+// initialize the first three file descriptors to
+// refer to the console device, up to NUM_FD,
+// the maximum number of file descriptors allocated to
+// each process.
