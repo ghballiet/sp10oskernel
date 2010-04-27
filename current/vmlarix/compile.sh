e6f78d605f582f29cb7681d6cfc5cbc37bb5d423
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-27 16:24:44 +0000

    added compile script

---
+++ current/vmlarix/compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+OLDPWD=$PWD
+COMPILE_ROOT="~/svn/sp10oskernel/current"
+MKDIRS="usr/ vmlarix/"
+
+cd $COMPILE_ROOT
+echo "Updating source from repository..."
+svn up
+
+for f in $MKDIRS; do
+    cd $f
+    echo "Compiling $f..."
+    make >/dev/null && echo "$f compiled succesfully." || (echo "ERROR!" && return -1)
+    cd $COMPILE_ROOT
+done
+
+cd $OLDPWD
