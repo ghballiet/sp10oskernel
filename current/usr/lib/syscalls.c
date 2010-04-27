de7565f599e48f798ef23d55e99edf65f3579da0
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-27 13:28:57 +0000

    debug syscalls

--- current/usr/lib/syscalls.c
+++ current/usr/lib/syscalls.c
@@ -16,6 +16,7 @@ ssize_t read(int fd, const void *buf, size_t count)
 
 ssize_t write(int fd, const void *buf, size_t count)
 {
+  printf("!!!!!!! - WRITE!\n\r")
   return (ssize_t) syscall(SYS_write,fd,buf,count);
 }
 
