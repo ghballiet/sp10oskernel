c56e8636132290c5b9a26376631eb21abb26001f
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-27 12:58:41 +0000

    uncommented to test build

--- current/vmlarix/process/syscall_handler.c
+++ current/vmlarix/process/syscall_handler.c
@@ -13,20 +13,19 @@ uint32_t c_SWI_handler(uint32_t syscall_num,
 			uint32_t p2, 
 			uint32_t p3)
 {
-  switch(syscall_num)
-    {
-/*     case SYS_open: */
-/*       return sys_open((char *)p1,p2,p3); */
-/*       break; */
-/*     case SYS_close: */
-/*       return sys_close(p1); */
-/*       break; */
-/*     case SYS_read: */
-/*       return sys_read(p1,(void *)p2,p3); */
-/*       break; */
-/*     case SYS_write: */
-/*       return sys_write(p1,(void *)p2,p3); */
-/*       break; */
+  switch(syscall_num) {
+      case SYS_open:
+       return sys_open((char *)p1,p2,p3); 
+       break; 
+     case SYS_close: 
+       return sys_close(p1); 
+       break; 
+     case SYS_read: 
+       return sys_read(p1,(void *)p2,p3); 
+       break; 
+     case SYS_write: 
+       return sys_write(p1,(void *)p2,p3); 
+       break; 
     default:
       return sys_undefined(syscall_num);
       break;
