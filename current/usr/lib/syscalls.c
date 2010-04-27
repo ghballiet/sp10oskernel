f0ae59d54b420f746e14fe3a2e6c8111c4e29dc0
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-27 13:45:16 +0000

    debugging syscalls

--- current/usr/lib/syscalls.c
+++ current/usr/lib/syscalls.c
@@ -16,8 +16,7 @@ ssize_t read(int fd, const void *buf, size_t count)
 
 ssize_t write(int fd, const void *buf, size_t count)
 {
-  printf("!!!!!!! - WRITE!\n\r")
-  return (ssize_t) syscall(SYS_write,fd,buf,count);
+  return (ssize_t) syscall(SYS_write,fd,buf,count);A
 }
 
 int open(const char *pathname, int flags, mode_t mode)
