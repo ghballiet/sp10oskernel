a0164da0c8e6e7e1ef86c6842fd267e98d3b5d4e
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-27 13:06:45 +0000

    debugging sys_open in syscall_handler.c

--- current/vmlarix/process/syscall_handler.c
+++ current/vmlarix/process/syscall_handler.c
@@ -17,18 +17,22 @@ uint32_t c_SWI_handler(uint32_t syscall_num,
       case SYS_open:
        return sys_open((char *)p1,p2,p3); 
        break; 
-     case SYS_close: 
-       return sys_close(p1); 
-       break; 
-     case SYS_read: 
-       return sys_read(p1,(void *)p2,p3); 
-       break; 
-     case SYS_write: 
-       return sys_write(p1,(void *)p2,p3); 
-       break; 
+     // case SYS_close: 
+     //   return sys_close(p1); 
+     //   break; 
+     // case SYS_read: 
+     //   return sys_read(p1,(void *)p2,p3); 
+     //   break; 
+     // case SYS_write: 
+     //   return sys_write(p1,(void *)p2,p3); 
+     //   break; 
     default:
       return sys_undefined(syscall_num);
       break;
     }
 }
 
+int sys_open(const char *pathname, int flags, mode_t mode) {
+  kprintf("SYSOPEN called with %s, %d and %s\n\r",pathname,flags,mode); 
+  return -1;
+}
