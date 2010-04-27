586908ef5954f488e0d43ac1471f5a9c17149d26
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-27 16:26:59 +0000

    updated compile script

--- current/vmlarix/compile.sh
+++ current/vmlarix/compile.sh
@@ -5,12 +5,12 @@ COMPILE_ROOT="/home/glhunt/svn/sp10oskernel/current"
 MKDIRS="usr/ vmlarix/"
 
 cd $COMPILE_ROOT && \
-echo "Updating source from repository..."
-svn up
+echo "Updating source from repository..." && \
+svn up && \
 
 for f in $MKDIRS; do
-    cd $f
-    echo "Compiling $f..."
+    cd $f && \
+    echo "Compiling $f..." && \
     make >/dev/null && echo "$f compiled succesfully." || (echo "ERROR!" && return -1)
     cd $COMPILE_ROOT
 done
