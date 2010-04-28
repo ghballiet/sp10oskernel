ffc8c66d855cf4287556c43b0c3d197a17271ed2
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-28 13:59:08 +0000

    debugging syscalls

--- current/vmlarix/process/syscall_handler.c
+++ current/vmlarix/process/syscall_handler.c
@@ -5,18 +5,12 @@
 // ====================
 // = define sys calls =
 // ====================
-
-int sys_open(const char *pathname, int flags, mode_t mode) {
-  kprintf("SYSOPEN called with %s, %d and %s\n\r",pathname,flags,mode); 
-  return -1;
-}
-
-int sys_read(int fd, const void *buf, size_t count) {
+ssize_t sys_read(int fd, const void *buf, size_t count) {
   kprintf("SYSREAD called with %d, %s and %d\n\r",fd, buf, count);
   return -1;
 }
 
-int sys_write(int fd, const void *buf, size_t count) {
+ssize_t sys_write(int fd, const void *buf, size_t count) {
   kprintf("SYSWRITE called with %d, %s and %d\n\r",fd, buf, count);
   return -1;
 }
@@ -38,9 +32,9 @@ uint32_t c_SWI_handler(uint32_t syscall_num,
 {
   kprintf("HANDLING SYSCALL: %d\n\r",syscall_num);
   switch(syscall_num) {
-      case SYS_open:
-       return sys_open((char *)p1,p2,p3); 
-       break; 
+      // case SYS_open:
+      //  return sys_open((char *)p1,p2,p3); 
+      //  break; 
      // case SYS_close: 
      //   return sys_close(p1); 
      //   break; 
