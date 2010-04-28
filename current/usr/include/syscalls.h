fb7bd3a689c0e3629cc7d195a774052dc500a6b4
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-28 13:57:56 +0000

    still working on syscalls

--- current/usr/include/syscalls.h
+++ current/usr/include/syscalls.h
@@ -7,9 +7,9 @@
 
 extern int syscall(uint32_t call_num, ...);
 
-
 int brk(void *end_data_segment);
 ssize_t write(int fd, const void *buf, size_t count);
+ssize_t read(int fd, const void *buf, size_t count);
 
 int open(const char *pathname, int flags, mode_t mode);
 /* int open(const char *pathname, int flags); */
