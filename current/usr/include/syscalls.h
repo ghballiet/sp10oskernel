fee1ca3ee641338a17a49c129d822873e343e02c
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-28 20:09:16 +0000

    added some functions to header file

--- current/usr/include/syscalls.h
+++ current/usr/include/syscalls.h
@@ -10,8 +10,9 @@ extern int syscall(uint32_t call_num, ...);
 int brk(void *end_data_segment);
 ssize_t write(int fd, const void *buf, size_t count);
 ssize_t read(int fd, const void *buf, size_t count);
-
 int open(const char *pathname, int flags, mode_t mode);
+int32_t sys_undefined(int32_t syscall_num);
+uint32_t c_SWI_handler(uint32_t syscall_num, uint32_t p1, uint32_t p2, uint32_t p3);
 /* int open(const char *pathname, int flags); */
 
 
