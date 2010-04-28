79d5eb5006f32c6e2b6d50b8c587ff33355fe21d
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-28 19:45:26 +0000

    removed changes

--- current/vmlarix/process/elf_load.c
+++ current/vmlarix/process/elf_load.c
@@ -9,7 +9,6 @@
    program entry point, or NULL on failure */
 void *elf_load(char *filename)
 {
-  kprintf("Loading %s\n\r",filename);
   int fd = vfs_open(filename, O_RDONLY, 0);
   Elf32_Ehdr ehdr;
   int size;
