fcbd5fb1b7ae93e865cc4190f43b06744c88f2cb
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-28 21:29:07 +0000

    testing elf_load

--- current/vmlarix/process/elf_load.c
+++ current/vmlarix/process/elf_load.c
@@ -47,7 +47,7 @@ void *elf_load(char *filename)
   int a;
   kprintf("Reading from %s:\n\r", filename);
   for(a=0;a<10;a++) {
-    kprintf("%d:\t%X\t%s\n\r",a,buf[a],buf[a]);
+    kprintf("%d:\t%X\n\r",a,buf[a]);
   }
   
   // move lseek back
