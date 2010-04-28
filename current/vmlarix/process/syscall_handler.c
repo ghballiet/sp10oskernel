e65311cfd568e20bc9a9e3b671286416f8fe8489
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-28 20:01:54 +0000

    debugging syscall_handler

--- current/vmlarix/process/syscall_handler.c
+++ current/vmlarix/process/syscall_handler.c
@@ -1,17 +1,22 @@
 #include <sys/syscall.h>
 #include <process.h>
-// #include <kprintf.h>
+#include <kprintf.h>
 
 // ====================
 // = define sys calls =
 // ====================
 ssize_t sys_read(int fd, const void *buf, size_t count) {
-  // kprintf("SYSREAD called with %d, %s and %d\n\r",fd, buf, count);
+  kprintf("SYSREAD called with %d, %s and %d\n\r",fd, buf, count);
   return -1;
 }
 
 ssize_t sys_write(int fd, const void *buf, size_t count) {
-  // kprintf("SYSWRITE called with %d, %s and %d\n\r",fd, buf, count);
+  kprintf("SYSWRITE called with %d, %s and %d\n\r",fd, buf, count);
+  return -1;
+}
+
+int sys_open(const char *pathname, int flags, mode_t mode) {
+  kprintf("SYSOPEN called with %s, %d and %s\n\r",pathname,flags,mode);
   return -1;
 }
 
@@ -21,7 +26,7 @@ ssize_t sys_write(int fd, const void *buf, size_t count) {
 
 int32_t sys_undefined(int32_t syscall_num)
 {
-  // kprintf("Undefined system call: %d\n\r",syscall_num);
+  kprintf("Undefined system call: %d\n\r",syscall_num);
   return syscall_num;
 }
 
@@ -30,14 +35,14 @@ uint32_t c_SWI_handler(uint32_t syscall_num,
 			uint32_t p2, 
 			uint32_t p3)
 {
-  // kprintf("HANDLING SYSCALL: %d\n\r",syscall_num);
+  kprintf("HANDLING SYSCALL: %d\n\r",syscall_num);
   switch(syscall_num) {
-      // case SYS_open:
-      //  return sys_open((char *)p1,p2,p3); 
-      //  break; 
-     // case SYS_close: 
-     //   return sys_close(p1); 
-     //   break; 
+      case SYS_open:
+       return sys_open((char *)p1,p2,p3); 
+       break; 
+     case SYS_close: 
+       return sys_close(p1); 
+       break; 
      case SYS_read: 
        return sys_read(p1,(void *)p2,p3); 
        break; 
