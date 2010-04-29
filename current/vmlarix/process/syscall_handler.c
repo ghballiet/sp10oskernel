ceb5b2f495ca60105a68cb9e43895cbbef909b66
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-29 16:55:35 +0000

    handling sys_write

--- current/vmlarix/process/syscall_handler.c
+++ current/vmlarix/process/syscall_handler.c
@@ -15,7 +15,7 @@ ssize_t sys_read(int fd, const void *buf, size_t count) {
 ssize_t sys_write(int fd, const void *buf, size_t count) {
   proc_rec *p = get_running_process();
   int pfd = p->fd[fd];
-  vfs_write_dev(pfd,buf,count);  
+  vfs_write(1,buf,count);  
   return (ssize_t)0;
 }
 
