5a570064f1bf6694100f35bc6a2602cd2b849d50
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-29 01:47:19 +0000

    debugging process.c

--- current/vmlarix/process/process.c
+++ current/vmlarix/process/process.c
@@ -41,7 +41,7 @@ void process_table_init()
 
 
 // initialize the process file descriptor table
-int32_t process_fd_create() {
+int32_t *process_fd_create() {
   // define p->fd as an array of ints
   // of size NUM_FD
   
