ccb9a24f8de7bf1cdae3d95dbb88ac556f43e7a3
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-04-29 01:40:06 +0000

    debugging process.c

--- current/vmlarix/process/process.c
+++ current/vmlarix/process/process.c
@@ -68,8 +68,8 @@ void process_fd_init(proc_rec p) {
     fds[i] = PROC_FD_DEFAULT;
   }
   
-  p->fd = (int32_t *)fds;
-  p->num_fd = (int32_t)PROC_NUM_FD;
+  p.fd = fds;
+  p.num_fd = (int32_t)PROC_NUM_FD;
 }
 
 /* Create a process by allocating a process table entry and
