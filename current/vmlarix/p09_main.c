aa1554b4f6d1fc049eae75866c9adce0e5a9bb52
glenrhunt <glenrhunt@1c6ee5be-1751-11df-8630-0728b3a1dc89>
2010-05-07 18:49:20 +0000

    added debug statements to p09_main

--- current/vmlarix/p09_main.c
+++ current/vmlarix/p09_main.c
@@ -94,6 +94,7 @@ int main()
   kprintf("Calling process_create...\n\r");
   init_prec = process_create(0, (void*)0, init_stack);
   // TODO: doesn't look like it's getting beyond this point
+  kprintf("Got past process_create...\n\r");
   init_prec->page_table = pt_new();
   kprintf("Got past pt_new...\n\r");
   init_prec->page_table_virt = (void*)phys_to_virt(kernel_pt,init_prec->page_table);
